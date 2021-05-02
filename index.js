87ccd809ecd35ffb09fe006cbc44959bad14db90
Michael Martinez <zeroeclipse@live.com>
2021-05-01 17:44:15 -0700

    commit tree 42a02cf7eed1157c2575ad3c1b153b02bacf73cd

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  string = string.split(":");
+  let hour = parseInt(string[0])
+  // let minute = parseInt(string[1])
+  if (hour > 12 && hour < 17) {
+    return "Good Afternoon"
+  }
+  else if (hour > 17) {
+    return "Good Evening"
+
+  }
+  else {
+    return "Good Morning"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  let node = document.querySelector("#greeting")
+  node.innerText = message
+}
